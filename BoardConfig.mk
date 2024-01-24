853f1dd1f79181fb1f780fc56f5804561a6778d4
lahaina <nattolecats@gmail.com>
2024-01-24 21:17:33 +0900

    veux_jp: Add custom framework compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,3 +9,4 @@ $(call inherit-product, device/xiaomi/veux/BoardConfig.mk)
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(LOCAL_PATH)/vintf/framework_compatibility_matrix.xml
